bc905c0d2f9ea8467cf66e5b95c07cfd9b2096e9
Aiden Storey <git@storey.dev>
2023-12-08 02:04:20 -0500

    Day 08

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 import collections
+import functools
 import math
 import re
 import time
@@ -524,8 +525,85 @@ def challenge_7_2(data : str) -> int:
     return result
 
 
+@challenge
+def challenge_8_1(data : str) -> int:
+    directions, nodes = data.split('\n\n')
+
+    re_letters : re.Pattern = re.compile('[A-Z]+')
+
+    directions = [
+        0 if direction == 'L' else 1
+        for direction in directions
+    ]
+
+    map : dict[str, str] = {
+        k: v
+        for k, *v in (re_letters.findall(node) for node in nodes.split('\n'))
+    }
+
+    current_location : str = 'AAA'
+    direction_index : int = 0
+
+    result : int = 0
+
+    while current_location != 'ZZZ':
+        direction = directions[direction_index]
+
+        current_location = map[current_location][direction]
+
+        direction_index = (direction_index + 1) % len(directions)
+        result += 1
+
+    return result
+
+
+@challenge
+def challenge_8_2(data : str) -> int:
+    directions, nodes = data.split('\n\n')
+
+    re_letters : re.Pattern = re.compile('[A-Z]+')
+
+    directions = [
+        0 if direction == 'L' else 1
+        for direction in directions
+    ]
+
+    map : dict[str, str] = {
+        k: v
+        for k, *v in (re_letters.findall(node) for node in nodes.split('\n'))
+    }
+
+    def __length(location : str) -> int:
+        length : int = 0
+        index : int = 0
+
+        while location[-1] != 'Z':
+            direction = directions[index]
+            location = map[location][direction]
+
+            index = (index + 1) % len(directions)
+            length += 1
+
+        return length
+
+    def __gcd(a : int, b : int) -> int:
+        while b:
+            a, b = b, a % b
+        return a
+
+    def __lcm(a : int, b : int) -> int:
+        return a * b // __gcd(a, b)
+
+    lengths : list[int] = [
+        __length(location)
+        for location in map.keys() if location[-1] == 'A'
+    ]
+
+    return functools.reduce(__lcm, lengths)
+
+
 def main() -> None:
-    runner(7)
+    runner(8)
 
 
 if __name__ == '__main__':
