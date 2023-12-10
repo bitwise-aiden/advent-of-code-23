5e88c6d44abf1c2019e71c9cfcc737dd74bf688f
Aiden Storey <git@storey.dev>
2023-12-10 03:11:24 -0500

    Day 10

--- main.py
+++ main.py
@@ -650,8 +650,162 @@ def challenge_9_1(data : str) -> int:
     return result
 
 
+class Vector():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+    def __neg__(self):
+        return Vector(-self.x, -self.y)
+
+    def __sub__(self, other):
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __str__(self):
+        return f'({self.x}, {self.y})'
+
+U = Vector(0, -1)
+D = Vector(0, 1)
+L = Vector(-1, 0)
+R = Vector(1, 0)
+Z = Vector(0, 0)
+I = Vector(100, 100)
+
+pipes : dict[str, tuple[Vector]] = {
+    '|': {U, D},
+    '-': {L, R},
+    'L': {U, R},
+    'J': {U, L},
+    '7': {D, L},
+    'F': {D, R},
+    '.': {I}
+}
+
+
+@challenge
+def challenge_10_1(data : str) -> int:
+    ground = [
+        [pipes.get(char, Z) for char in line]
+        for line in data.split('\n')
+    ]
+
+    s_loc = data.replace('\n', '').find('S')
+    s_loc = Vector(s_loc % len(ground[0]), s_loc // len(ground[0]))
+
+    directions = []
+
+    for o in [U, D, L, R]:
+        c = o + s_loc
+
+        if  -o in ground[c.y][c.x]:
+            directions.append(o)
+
+
+    locations = [s_loc + d for d in directions]
+    distance = 1
+
+    while locations[0] != locations[1]:
+        directions = [
+            list(ground[l.y][l.x] - {-d})[0]
+            for d, l in zip(directions, locations)
+        ]
+
+        locations = [
+            l + d
+            for d, l in zip(directions, locations)
+        ]
+
+        distance += 1
+
+    return distance
+
+
+@challenge
+def challenge_10_2(data : str) -> int:
+    dirs = [U, R, D, L]
+
+    ground = [
+        [pipes.get(char, {Z}) for char in line]
+        for line in data.split('\n')
+    ]
+
+    # Find start location
+    s_loc = data.replace('\n', '').find('S')
+    s_loc = Vector(s_loc % len(ground[0]), s_loc // len(ground[0]))
+
+    # Find directions that connect to start
+    directions = []
+    for o in [U, D, L, R]:
+        c = o + s_loc
+
+        if -o in ground[c.y][c.x]:
+            directions.append(o)
+
+    direction = directions[0]
+    ground[s_loc.y][s_loc.x] = set(directions)
+    location = s_loc + direction
+
+
+    # Store out all path locations
+    path = {location}
+    while location != s_loc:
+        direction = list(ground[location.y][location.x] - {-direction})[0]
+        location += direction
+
+        path.add(location)
+
+    def process_pipe(check_location : Vector) -> (Vector, int):
+        if check_location not in path:
+            return check_location, 0
+
+        if R not in ground[check_location.y][check_location.x]:
+            return check_location, 1
+
+        exit_location : Vector = check_location + R
+
+        while {L, R} == ground[exit_location.y][exit_location.x]:
+            exit_location += R
+
+        shared_count : int = len(
+            ground[check_location.y][check_location.x] &
+            ground[exit_location.y][exit_location.x]
+        )
+
+        return exit_location, (shared_count + 1) % 2
+
+    result = 0
+
+    for x in range(len(ground[0])):
+        for y in range(len(ground)):
+            l = Vector(x, y)
+
+            if l in path:
+                continue
+
+            count = 0
+
+            while l.x < len(ground[0]):
+                l, c = process_pipe(l + R)
+
+                count += c
+
+            if count % 2 == 1:
+                result += 1
+
+    return result
+
+
 def main() -> None:
-    runner(9)
+    runner(10)
 
 
 if __name__ == '__main__':
