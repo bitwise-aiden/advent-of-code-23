86dedd6f358ba25a41793be7360d2f7d5ac8dd84
Aiden Storey <git@storey.dev>
2023-12-14 22:27:26 -0500

    Day 14

--- main.py
+++ main.py
@@ -655,6 +655,18 @@ class Vector():
         self.x = x
         self.y = y
 
+    def x_get(self):
+        return self.x
+
+    def x_set(self, value):
+        self.x = value
+
+    def y_get(self):
+        return self.y
+
+    def y_set(self, value):
+        self.y = value
+
     def __add__(self, other):
         return Vector(self.x + other.x, self.y + other.y)
 
@@ -990,8 +1002,36 @@ def challenge_13_2(data : str) -> int:
     return result
 
 
+@challenge
+def challenge_14_1(data : str) -> int:
+    result : int = 0
+
+    columns : dict[int, int] = {}
+    lines : list[str] = data.split('\n')
+    size : int = len(lines)
+
+    for i, line in enumerate(lines):
+        for j, char in enumerate(line):
+            curr : int = columns.get(j, 0)
+
+            match char:
+                case 'O':
+                    result += size - curr
+                    columns[j] = curr + 1
+                case '#':
+                    columns[j] = i + 1
+
+    return result
+
+@challenge
+def challenge_14_2(data : str) -> int:
+    result : int = 0
+
+    return result
+
+
 def main() -> None:
-    runner(13)
+    runner(14)
 
 
 if __name__ == '__main__':
