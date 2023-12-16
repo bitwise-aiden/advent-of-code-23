36d6997fd65a024f7762a4d0354e158042c4abc9
Aiden Storey <git@storey.dev>
2023-12-16 11:02:59 -0500

    Day 16

--- main.py
+++ main.py
@@ -1023,8 +1023,6 @@ def challenge_14_1(data : str) -> int:
 
     return result
 
-grid = tuple()
-
 
 @challenge
 def challenge_14_2(data : str) -> int:
@@ -1071,8 +1069,85 @@ def challenge_15_2(data : str) -> int:
     return result
 
 
+ch16_pipes : dict = {
+    '|': ({U, D}),
+    '-': ({L, R}),
+    '/': ({L, U}, {R, D}),
+    '\\': ({L, D}, {R, U}),
+}
+
+def __challenge_16_filled(grid, position, direction):
+    height = len(grid)
+    width = len(grid[0])
+
+    seen = set()
+    todo = [(position, direction)]
+
+    while todo:
+        p, d = todo.pop()
+
+        if not (0 <= p.x < width and 0 <= p.y < height):
+            continue
+
+        if (p, d) in seen:
+            continue
+        seen.add((p, d))
+
+        t = grid[p.y][p.x]
+
+        match t:
+            case '.':
+                todo.append((p + d, d))
+            case '|' | '-':
+                ss = ch16_pipes[t]
+                if ss in ch16_pipes[t]:
+                    todo.append((p + d, d))
+                else:
+                    for s in ss:
+                        todo.append((p + s, s))
+            case '\\' | '/':
+                for ss in ch16_pipes[t]:
+                    if -d in ss:
+                        ed = (ss - {-d}).pop()
+                        todo.append((p + ed, ed))
+
+    cells = {p for p, _ in seen}
+
+    return len(cells)
+
+
+
+@challenge
+def challenge_16_1(data : str) -> int:
+    grid = [list(line) for line in data.split('\n')]
+    return __challenge_16_filled(grid, Z, R)
+
+
+@challenge
+def challenge_16_2(data : str) -> int:
+    result : int = 0
+
+    grid = [list(line) for line in data.split('\n')]
+    height = len(grid)
+    width = len(grid[0])
+
+    for y in range(height):
+        l, r = Vector(0, y), Vector(width - 1, y)
+
+        result = max(result, __challenge_16_filled(grid, l, R))
+        result = max(result, __challenge_16_filled(grid, r, L))
+
+    for x in range(width):
+        t, b = Vector(x, 0), Vector(x, height - 1)
+
+        result = max(result, __challenge_16_filled(grid, t, D))
+        result = max(result, __challenge_16_filled(grid, b, U))
+
+    return result
+
+
 def main() -> None:
-    runner(15)
+    runner(16)
 
 
 if __name__ == '__main__':
