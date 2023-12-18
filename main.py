8ac3a3dc0680ac27b93a9a8316f9c9b3eef6d1a1
Aiden Storey <git@storey.dev>
2023-12-18 03:05:54 -0500

    Day 18

--- main.py
+++ main.py
@@ -684,7 +684,7 @@ class Vector():
         return self.x == other.x and self.y == other.y
 
     def __lt__(self, other):
-        return self.x < other.x and self.y < other.y
+        return self.x <= other.x and self.y <= other.y
 
     def __hash__(self):
         return hash((self.x, self.y))
@@ -1220,9 +1220,52 @@ def challenge_17_2(data : str) -> int:
     return __challenge_17_process(data, 4, 10)
 
 
+def __challenge_18_shoelace(points : list[Vector]) -> int:
+    x = [v.x for v in points]
+    y = [v.y for v in points]
+
+    return abs(
+        sum(i * j for i, j in zip(x, y[1:] + y[:1]))
+        - sum(i * j for i, j in zip(x[1:] + x[:1], y))
+    ) / 2
+
+
+def __challenge_18_data(data : str, processor) -> int:
+    re_data : re.Pattern = re.compile('([A-Z]) ([0-9]+) \(#(.*?)\)')
+
+    polygon : list[Vector] = [Z]
+    dist = 0
+
+    for line in data.split('\n'):
+        dir, dis, col = re_data.match(line).groups()
+        dir, dis = processor(dir, dis, col)
+        dist += dis
+
+        polygon.append(polygon[-1] + dir * dis)
+
+    return int(__challenge_18_shoelace(polygon) + dist // 2 + 1)
+
+
+@challenge
+def challenge_18_1(data : str) -> int:
+    directions = {'U': U, 'D': D, 'L': L, 'R': R}
+    return __challenge_18_data(
+        data,
+        lambda dir, dis, _: (directions[dir], int(dis)),
+    )
+
+@challenge
+def challenge_18_2(data : str) -> int:
+    directions = [R, U, L, D]
+    return __challenge_18_data(
+        data,
+        lambda _1, _2, col: (directions[int(col[-1], 16)], int(col[:5], 16)),
+    )
+
+
 def main() -> None:
     try:
-        runner(17)
+        runner(18)
     except KeyboardInterrupt:
         print('\rexiting...')
 
